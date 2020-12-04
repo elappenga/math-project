f746afa6fde6769051e768eedb0766e09cc9ff3d
Reggie <vassarjonez@yahoo.com>
2020-12-03 21:52:24 -0500

    subracting

---
+++ script.js
@@ -0,0 +1,6 @@
+function subtractNumbers() {
+    var val1 = parseInt(document.getElementById("value1").value);
+    var val2 = parseInt(document.getElementById("value2").value);
+    var ansD = document.getElementById("answer");
+    ansD.value = val1 - val2;
+}
